6820e7fba6616c84fd4d95c2c65a42eb419ef4da
stacytalbot <stacymtalbot@gmail.com>
2018-11-20 11:16:26 +0000

    Add copyright to footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -6,16 +6,32 @@
       </a>
 
       <div class="flex flex-wrap flex-center">
-        <a href="https://geobon.org" title="Visit GEO BON" target="_blank">
-          <img src="../assets/images/logo-geobon.svg" alt="GEO BON logo" class="footer__partner-logo logo--geobon" />
-        </a>
-        <a href="http://proteuspartners.org" title="Visit Proteus Partners" target="_blank">
-          <img src="../assets/images/logo-proteus.svg" alt="Proteus logo" class="footer__partner-logo logo--proteus" />
-        </a>
-        <a href="https://www.unep-wcmc.org" title="Visit the UN Environment World Conservation Montioring Centre" target="_blank">
-          <img src="https://s3.amazonaws.com/wcmc.logo/UN-Environment-WCMC+logo+2017+white.svg" alt="UN Environment World Conservation Monitoring logo" class="footer__partner-logo logo--wcmc" />
-        </a>
+        <div class="flex flex-wrap flex-center">
+          <a href="https://geobon.org" title="Visit GEO BON" target="_blank">
+            <img src="../assets/images/logo-geobon.svg" alt="GEO BON logo" class="footer__partner-logo logo--geobon" />
+          </a>
+          <a href="http://proteuspartners.org" title="Visit Proteus Partners" target="_blank">
+            <img src="../assets/images/logo-proteus.svg" alt="Proteus logo" class="footer__partner-logo logo--proteus" />
+          </a>
+          <a href="https://www.unep-wcmc.org" title="Visit the UN Environment World Conservation Montioring Centre" target="_blank">
+            <img src="https://s3.amazonaws.com/wcmc.logo/UN-Environment-WCMC+logo+2017+white.svg" alt="UN Environment World Conservation Monitoring logo" class="footer__partner-logo logo--wcmc" />
+          </a>
+        </div>
+
+        <span class="footer__copyright">&copy; copyright {{ currentYear }}</span>
       </div>
     </div>
   </footer>
 </template>
+
+<script>
+  export default {
+    name: 'site-footer',
+
+    computed: {
+      currentYear () {
+        return new Date().getFullYear()
+      }
+    }
+  }
+</script>
