24858ff70be26930c0d66b6be32fabe7e3c09edd
maxime <maxycle@hotmail.com>
2022-03-16 11:12:25 +0000

    fix: update text for landing page

--- src/config.js
+++ src/config.js
@@ -3,34 +3,34 @@ export const externalSites = [
     id: 1,
     name: 'Ocean+ Library',
     link: 'https://data.oceanplus.org',
-    description: '[Ocean+ Library] addresses this need for information by providing an overview of global marine and coastal datasets of biodiversity importance, including some datasets of regional interest.',
+    description: '[Ocean+ Library] is a meta-database that provides summary information on additional online resources and data of ocean biodiversity importance. Users can search the information on the platform to find relevant resources that support effective ocean conservation on a regional and global level.',
     imagePath: require('@/assets/images/logo-ocean-plus-library.svg'),
   },
   {
     id: 2,
     name: 'Ocean+ Habitats',
     link: 'https://habitats.oceanplus.org/',
-    description: '[Ocean+ Habitats] aims to improve access to marine habitat data and information through partnerships, and will be evolving to include regional and national information.',
+    description: '[Ocean+ Habitats] provides readily-available information and statistics on the distribution and conservation status of critical marine and coastal habitats. Combining habitat distribution data with Protected Planet and the IUCN Red List, users can explore and download tabular and spatial data to inform the conservation of marine habitats.',
     imagePath: require('@/assets/images/logo-ocean-plus-habitats.svg'),
   },
-  {
-    id: 3,
-    name: 'Protected Planet – Marine',
-    link: 'https://www.protectedplanet.net/marine',
-    description: '[Protected Planet – Marine] presents the most recent official coverage statistics for marine protected areas, updated monthly from the World Database on Protected Areas.',
-    imagePath: require('@/assets/images/protected-planet-logo-reversed.svg'),
-  },
+  // {
+  //   id: 3,
+  //   name: 'Protected Planet – Marine',
+  //   link: 'https://www.protectedplanet.net/marine',
+  //   description: '[Protected Planet – Marine] presents the most recent official coverage statistics for marine protected areas, updated monthly from the World Database on Protected Areas.',
+  //   imagePath: require('@/assets/images/protected-planet-logo-reversed.svg'),
+  // },
   {
     id: 4,
     name: 'Ocean Data Viewer',
     link: 'http://data.unep-wcmc.org',
-    description: 'The [Ocean Data Viewer] offers users the opportunity to view and download a range of spatial datasets that are useful for informing decisions regarding the conservation of marine and coastal biodiversity.',
+    description: '[Ocean+ Data Viewer] is the go-to-platform for up-to-date, globally coherent, marine biodiversity spatial datasets. Users can view and download a range of spatial datasets that are useful for informing decisions regarding the conservation of ocean biodiversity.',
     imagePath: require('@/assets/images/odv-reversed-01.svg'),
   }
 ]
 
 export const description = [
-  'Home to more than half of all life on Earth, the Ocean covers over 70% of the surface of the planet.',
-  'The ocean contains some of the most productive ecosystems, vast natural resources, and unique habitats on Earth. The ocean also plays a vital role in regulating the planet’s climate. However, the marine environment is facing many pressures, from the impacts of climate change and overfishing to the effects of pollution from microplastics and marine litter.',
-  'Accurate and up-to-date data and information are required to support adaptive conservation and resource management that can help to mitigate these impacts, and to conserve marine species and habitats.'
+  'Ocean+ is an initiative that brings together accurate and up-to-date data and information on ocean biodiversity to support effective decisions that protect global marine life.',
+  'The ocean contains some of the most productive ecosystems, vast natural resources, and unique habitats on Earth. Home to more than half of all life on Earth, the ocean covers over 70% of the surface of the planet and plays a vital role in regulating the planet’s climate. Today, the marine environment is facing many pressures as a result of irresponsible human activity, from the impacts of climate change and overfishing to the effects of coastal development and pollution.',
+  'We must protect our ocean. Ocean+ and its three data platforms aim to provide open and trusted information on marine biodiversity so that we can all make the best choices to sustainably manage, conserve and restore the ocean.'
 ]
