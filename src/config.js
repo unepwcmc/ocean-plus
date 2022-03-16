570c9326595729a45776d09bef5e1546f28b0ff6
maxime <maxycle@hotmail.com>
2022-03-16 11:47:08 +0000

    fix: change links order

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const externalSites = [
   {
-    id: 1,
-    name: 'Ocean+ Library',
-    link: 'https://data.oceanplus.org',
-    description: '[Ocean+ Library] is a meta-database that provides summary information on additional online resources and data of ocean biodiversity importance. Users can search the information on the platform to find relevant resources that support effective ocean conservation on a regional and global level.',
-    imagePath: require('@/assets/images/logo-ocean-plus-library.svg'),
+    id: 4,
+    name: 'Ocean Data Viewer',
+    link: 'http://data.unep-wcmc.org',
+    description: '[Ocean+ Data Viewer] is the go-to-platform for up-to-date, globally coherent, marine biodiversity spatial datasets. Users can view and download a range of spatial datasets that are useful for informing decisions regarding the conservation of ocean biodiversity.',
+    imagePath: require('@/assets/images/odv-reversed-01.svg'),
   },
   {
     id: 2,
@@ -13,6 +13,13 @@ export const externalSites = [
     description: '[Ocean+ Habitats] provides readily-available information and statistics on the distribution and conservation status of critical marine and coastal habitats. Combining habitat distribution data with Protected Planet and the IUCN Red List, users can explore and download tabular and spatial data to inform the conservation of marine habitats.',
     imagePath: require('@/assets/images/logo-ocean-plus-habitats.svg'),
   },
+  {
+    id: 1,
+    name: 'Ocean+ Library',
+    link: 'https://data.oceanplus.org',
+    description: '[Ocean+ Library] is a meta-database that provides summary information on additional online resources and data of ocean biodiversity importance. Users can search the information on the platform to find relevant resources that support effective ocean conservation on a regional and global level.',
+    imagePath: require('@/assets/images/logo-ocean-plus-library.svg'),
+  },
   // {
   //   id: 3,
   //   name: 'Protected Planet – Marine',
@@ -20,13 +27,6 @@ export const externalSites = [
   //   description: '[Protected Planet – Marine] presents the most recent official coverage statistics for marine protected areas, updated monthly from the World Database on Protected Areas.',
   //   imagePath: require('@/assets/images/protected-planet-logo-reversed.svg'),
   // },
-  {
-    id: 4,
-    name: 'Ocean Data Viewer',
-    link: 'http://data.unep-wcmc.org',
-    description: '[Ocean+ Data Viewer] is the go-to-platform for up-to-date, globally coherent, marine biodiversity spatial datasets. Users can view and download a range of spatial datasets that are useful for informing decisions regarding the conservation of ocean biodiversity.',
-    imagePath: require('@/assets/images/odv-reversed-01.svg'),
-  }
 ]
 
 export const description = [
